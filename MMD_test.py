35ac50f543e7dff5d2ba6428978e2847065826d5
Mevlana Gemici <mevlanagemici@gmail.com>
2019-03-01 23:46:31 +0300

    bv

--- MMD_test.py
+++ MMD_test.py
@@ -9,6 +9,8 @@ import pdb
 import numpy as np
 import math 
 from random import shuffle 
+from pathlib import Path
+import os
 
 batch_size = 1000
 input_dim = 1
@@ -21,27 +23,30 @@ x = tf.placeholder(tf.float32, [None, input_dim])
 epsilon = tf.placeholder(tf.float32, [None, latent_dim])
 z_prior = tf.placeholder(tf.float32, [None, latent_dim])
 
-# x = tf.random_normal((batch_size, input_dim), 0, 1, dtype=tf.float32)
-# epsilon = tf.random_normal((batch_size, latent_dim), 0, 1, dtype=tf.float32)
-# z_prior = tf.random_normal((batch_size, latent_dim), 0, 1, dtype=tf.float32)
+tile_rate = 784
+x_input = tf.tile(x, [1, tile_rate])
 
-layer_1 = tf.layers.dense(inputs = tf.concat([x, epsilon], axis=1), units = 500, use_bias = True, activation = tf.nn.relu)
+# layer_1 = tf.layers.dense(inputs = tf.concat([x_input, epsilon], axis=1), units = 1000, use_bias = True, activation = tf.nn.relu)
+layer_1 = tf.layers.dense(inputs = tf.concat([x_input, epsilon], axis=1), units = 500, use_bias = True, activation = tf.nn.relu)
 layer_2 = tf.layers.dense(inputs = layer_1, units = 500, use_bias = True, activation = tf.nn.relu)
-layer_3 = tf.layers.dense(inputs = layer_2, units = 500, use_bias = True, activation = tf.nn.relu)
-layer_4 = tf.layers.dense(inputs = layer_3, units = 500, use_bias = True, activation = tf.nn.relu)
-z = tf.layers.dense(inputs = layer_4, units = latent_dim, use_bias = True, activation = None)
+# layer_3 = tf.layers.dense(inputs = layer_2, units = 500, use_bias = True, activation = tf.nn.relu)
+# layer_4 = tf.layers.dense(inputs = layer_3, units = 500, use_bias = True, activation = tf.nn.relu)
+z = tf.layers.dense(inputs = layer_2, units = latent_dim, use_bias = True, activation = None)
 
+# layer_1_rec = tf.layers.dense(inputs = z, units = 1000, use_bias = True, activation = tf.nn.relu)
 layer_1_rec = tf.layers.dense(inputs = z, units = 500, use_bias = True, activation = tf.nn.relu)
 layer_2_rec = tf.layers.dense(inputs = layer_1_rec, units = 500, use_bias = True, activation = tf.nn.relu)
-layer_3_rec = tf.layers.dense(inputs = layer_2_rec, units = 500, use_bias = True, activation = tf.nn.relu)
-layer_4_rec = tf.layers.dense(inputs = layer_3_rec, units = 500, use_bias = True, activation = tf.nn.relu)
-x_rec = tf.layers.dense(inputs = layer_4_rec, units = input_dim, use_bias = True, activation = None)
+# layer_3_rec = tf.layers.dense(inputs = layer_2_rec, units = 500, use_bias = True, activation = tf.nn.relu)
+# layer_4_rec = tf.layers.dense(inputs = layer_3_rec, units = 500, use_bias = True, activation = tf.nn.relu)
+x_rec = tf.layers.dense(inputs = layer_2_rec, units = input_dim*tile_rate, use_bias = True, activation = None)
 
-rec_cost = tf.reduce_mean(tf.reduce_sum((x_rec-x)**2, axis=1))
+rec_cost = tf.reduce_mean(helper.safe_tf_sqrt(tf.reduce_sum((x_rec-x_input)**2, axis=1)))
 MMD = helper.compute_MMD(z, z_prior, positive_only=True)
 
-start, timescale = 10000, 3
-cost = MMD+helper.hardstep((iter_tf-float(start))/float(timescale))*rec_cost
+start, timescale = 200, 1500
+# start, timescale = 0, 1
+lambda_z_comp = helper.hardstep((iter_tf-float(start))/float(timescale))
+cost = MMD+lambda_z_comp*rec_cost
 
 optimizer = tf.train.AdamOptimizer(learning_rate=0.0001, beta1=0.9, beta2=0.999, epsilon=1e-08)
 cost_step = optimizer.minimize(cost)
@@ -50,17 +55,24 @@ init = tf.initialize_all_variables()
 sess = tf.InteractiveSession()  
 sess.run(init)
 
+if not os.path.exists(str(Path.home())+'/datasets/MMD_test/'): os.makedirs(str(Path.home())+'/datasets/MMD_test/')
+file_path = str(Path.home())+'/datasets/MMD_test/results.txt'
+with open(file_path, "w") as text_file:
+    text_file.write('')
+
 for i in range(0, 100000):
     fd = {iter_tf: i, lambda_z: 1, x: np.random.randn(batch_size, input_dim), epsilon: np.random.randn(batch_size, latent_dim), z_prior: np.random.randn(batch_size, latent_dim)}
-    _, cost_np, z_np, z_prior_np = sess.run([cost_step, cost, z, z_prior], feed_dict = fd)
-    if i % 20 == 0:
-        print("Iteration: "+str(i) + " Cost: "+str(cost_np))
+    _, cost_np, MMD_np, rec_cost_np, lambda_z_comp_np, z_np, z_prior_np = sess.run([cost_step, cost, MMD, rec_cost, lambda_z_comp, z, z_prior], feed_dict = fd)
+    if i % 50 == 0:
+        print("Iteration: "+str(i) + " Cost: "+str(cost_np) + " MMD_np: "+str(MMD_np) + " lambda_z_comp_np: "+str(lambda_z_comp_np) + " rec_cost_np: "+str(rec_cost_np))
+        with open(file_path, "a") as text_file:
+            text_file.write(str(i) + ', '+ str(cost_np) + ', ' +str(MMD_np) + ', ' + str(rec_cost_np) + ', ' + str(lambda_z_comp_np)+'\n')
 
     if i % 100 == 0:
         fd = {iter_tf: i, lambda_z: 1, x: np.random.randn(10000, input_dim), epsilon: np.random.randn(10000, latent_dim), z_prior: np.random.randn(10000, latent_dim)}
         z_np, z_prior_np = sess.run([z, z_prior], feed_dict = fd)
-        helper.dataset_plotter([z_np,], colors=['r',], point_thickness = 4, save_dir = '~/MMD_posterior/', postfix = '_MMD_posterior_'+str(i)+'_e', postfix2 = '_MMD_posterior'+'_m')
-        helper.dataset_plotter([z_prior_np,], colors=['g',], point_thickness = 4, save_dir = '~/MMD_prior/', postfix = '_MMD_prior_'+str(i)+'_e', postfix2 = '_MMD_prior'+'_m')
-        helper.dataset_plotter([z_np, z_prior_np], point_thickness = 4, save_dir = '~/MMD_prior_posterior/', postfix = '_MMD_prior_posterior_'+str(i)+'_e', postfix2 = '_MMD_prior_posterior'+'_m')
+        helper.dataset_plotter([z_np,], colors=['r',], point_thickness = 4, save_dir = str(Path.home())+'/datasets/MMD_test/MMD_posterior/', postfix = '_MMD_posterior_'+str(i)+'_e', postfix2 = '_MMD_posterior'+'_m')
+        helper.dataset_plotter([z_prior_np,], colors=['g',], point_thickness = 4, save_dir = str(Path.home())+'/datasets/MMD_test/MMD_prior/', postfix = '_MMD_prior_'+str(i)+'_e', postfix2 = '_MMD_prior'+'_m')
+        helper.dataset_plotter([z_np, z_prior_np], point_thickness = 4, save_dir = str(Path.home())+'/datasets/MMD_test/MMD_prior_posterior/', postfix = '_MMD_prior_posterior_'+str(i)+'_e', postfix2 = '_MMD_prior_posterior'+'_m')
 
 pdb.set_trace()
---
+++ ploter_mmd.py
@@ -0,0 +1,157 @@
+import math
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+import pdb
+import scipy 
+from os import listdir
+import os
+import os.path
+import glob
+from os.path import isfile, join
+import subprocess
+import re
+from pathlib import Path
+import csv
+
+plt.style.use('seaborn-white')
+plt.rcParams['font.family'] = 'serif'
+plt.rcParams['font.serif'] = 'Times New Roman'
+plt.rcParams['font.monospace'] = 'Ubuntu Mono'
+plt.rcParams['font.size'] = 16
+plt.rcParams['font.weight'] = 'bold'
+plt.rcParams['axes.labelsize'] = 17
+plt.rcParams['axes.labelweight'] = 'normal'
+plt.rcParams['xtick.labelsize'] = 14
+plt.rcParams['ytick.labelsize'] = 14
+plt.rcParams['legend.fontsize'] = 16
+# plt.rcParams['axes.titlesize'] = 10
+# plt.rcParams['legend.fontweight'] = 'normal'
+# plt.rcParams['figure.titlesize'] = 12
+
+my_dpi = 200
+fig_width = 9*200
+fig_height = 6*200
+
+file_path = str(Path.home())+'/datasets/MMD_test/results.txt'
+
+# save_index = -1
+# # mode = 'LeastEpochs'
+# # mode = 'Full'
+# mode = 'EpochInterval'
+# min_epoch = 200
+# max_epoch = 1500
+# list_of_np_numerics = []
+
+# if os.path.exists(file_path): 
+#     with open(file_path, "r") as text_file: data_lines = text_file.readlines()
+#     all_numeric_lines = []
+#     if len(data_lines)==1: 
+#         data_lines_corrected = data_lines[0].split('Epoch')[1:]
+#         data_lines = ['Epoch'+e for e in data_lines_corrected]
+
+#     for data_line in data_lines:
+#         numerics = []
+#         for e in re.split(': | |\n|',data_line):
+#             try: 
+#                 float_e = float(e)
+#                 numerics.append(float_e)
+#             except: 
+#                 pass
+#         all_numeric_lines.append(numerics)
+#     np_all_numeric_lines = np.asarray(all_numeric_lines)
+#     list_of_np_numerics.append(np_all_numeric_lines)
+
+all_data = {'i': [], 'MMD': [], 'Rec': [], 'Overall': [], 'Lambda': []}
+with open(file_path) as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    for row in csv_reader:
+        all_data['i'].append(float(row[0]))
+        all_data['Overall'].append(float(row[1]))
+        all_data['MMD'].append(float(row[2]))
+        all_data['Rec'].append(float(row[3]))
+        all_data['Lambda'].append(float(row[4]))
+
+# identifiers = ['Lambda', 'Rec', 'MMD', 'Overall']
+identifiers = ['Lambda', 'Rec', 'MMD']
+colors = ['g', 'r', 'b', 'k']
+markers = ['v', 'h',  'o', 's']
+
+# plt.figure(figsize=(fig_width/my_dpi, fig_height/my_dpi), dpi=my_dpi)
+plt.figure(figsize=(fig_width/(2*my_dpi), fig_height/my_dpi), dpi=my_dpi)
+plt.cla()
+y_label = 'MMD Test'
+x_label = 'Training Iterations'
+min_y_val = 100000000000000
+max_y_val = -100000000000000
+for i, identifier in enumerate(identifiers):
+    x_vals = all_data['i']
+    y_vals = all_data[identifier]
+    if min(y_vals)<min_y_val: min_y_val = min(y_vals)
+    if max(y_vals)>max_y_val: max_y_val = max(y_vals)
+    plt.plot(x_vals, y_vals, linewidth=2, linestyle='-', color=colors[i], label=identifiers[i], marker=markers[i], markersize=10)
+
+y_range = (max_y_val-min_y_val)
+plt.ylabel(y_label, fontsize=16)
+plt.xlabel(x_label, fontsize=16)
+plt.grid()
+plt.legend(frameon=True)
+plt.ylim((min_y_val-0.1*y_range, max_y_val+0.1*y_range ))
+plt.xlim((0,1000))
+plt.savefig(str(Path.home())+'/datasets/MMD_test/results.png', bbox_inches='tight', format='png', dpi=my_dpi, transparent=False)
+print('Saving to path: ', str(Path.home())+'/datasets/MMD_test/results.png')
+
+# # plt.figure(figsize=(fig_width/my_dpi, fig_height/my_dpi), dpi=my_dpi)
+# plt.figure(figsize=(fig_width/(2*my_dpi), fig_height/my_dpi), dpi=my_dpi)
+# plt.cla()
+# y_label = 'Inception Score (IS)'
+# x_label = 'Training Epochs'
+# min_y_val = 100000000000000
+# max_y_val = -100000000000000
+# for i, np_all_numeric_lines in enumerate(list_of_np_numerics):
+#     if mode == 'LeastEpochs':
+#         mask = np_all_numeric_lines[1:,0]<=least_max_epoch
+#         x_vals = np_all_numeric_lines[1:,0][mask]
+#         y_vals = np_all_numeric_lines[1:,2][mask]
+#     elif mode == 'EpochInterval':        
+#         mask_upper = np_all_numeric_lines[1:,0]<=max_epoch
+#         mask_lower = np_all_numeric_lines[1:,0]>=min_epoch        
+#         mask = mask_upper*mask_lower
+#         x_vals = np_all_numeric_lines[1:,0][mask]
+#         y_vals = np_all_numeric_lines[1:,2][mask]
+#     else:
+#         x_vals = np_all_numeric_lines[1:,0]
+#         y_vals = np_all_numeric_lines[1:,2]
+#     if np.min(y_vals)<min_y_val: min_y_val = np.min(y_vals)
+#     if np.max(y_vals)>max_y_val: max_y_val = np.max(y_vals)
+#     plt.plot(x_vals, y_vals, linewidth=2, linestyle='-', color=colors[i], label=identifiers[i], marker=markers[i], markersize=10)
+
+# # i=3
+# # np_all_numeric_lines=list_of_np_numerics[0]
+# # if mode == 'LeastEpochs':
+# #     mask = np_all_numeric_lines[1:,0]<=least_max_epoch
+# #     x_vals = np_all_numeric_lines[1:,0][mask]
+# #     y_vals = np_all_numeric_lines[1:,4][mask]
+# # else:
+# #     x_vals = np_all_numeric_lines[1:,0]
+# #     y_vals = np_all_numeric_lines[1:,4]
+# # if np.min(y_vals)<min_y_val: min_y_val = np.min(y_vals)
+# # if np.max(y_vals)>max_y_val: max_y_val = np.max(y_vals)
+# # plt.plot(x_vals, y_vals, linewidth=2, linestyle='-', color=colors[i], label=identifiers[i], marker=markers[i], markersize=10)
+
+# y_range = (max_y_val-min_y_val)
+# plt.ylabel(y_label, fontsize=16)
+# plt.xlabel(x_label, fontsize=16)
+# plt.grid()
+# plt.legend(frameon=True)
+# plt.ylim((min_y_val-0.1*y_range, max_y_val+0.4*y_range ))
+# plt.xlim((0,1000))
+# plt.savefig(exp_folders[save_index]+'Visualization/is_comparison.png', bbox_inches='tight', format='png', dpi=my_dpi, transparent=False)
+# print('Saving to path: ', exp_folders[save_index]+'Visualization/is_comparison.png')
+
+# plt.close('all')
+
+
+
