086ad7ae4c5d9e9d762bbf280980ee103f9fae70
Mevlana Gemici <mevlanagemici@gmail.com>
2019-05-06 01:49:42 +0300

    bv

--- gan_reg.py
+++ gan_reg.py
@@ -55,7 +55,7 @@ if Algorithm == 'RNF':
                              'encoder_mode': 'Deterministic', 'divergence_mode': 'None', 'dual_dist_mode': '',  'infomax_mode': 'None',
                              'enc_normalization_mode': 'Layer Norm', 'gen_normalization_mode': 'Layer Norm', 'cri_normalization_mode': 'None', 
                              'enc_reg_strength': 0, 'enc_n_slice_dir': 1, 'enc_inv_MMD_n_reflect': 0, 'enc_inv_MMD_n_trans': 0, 'enc_inv_MMD_strength': 0,
-                             'critic_reg_mode': [], 'cri_reg_strength': 0, 'lambda_mix': 0, 'timers': {}, 'rnf_prop': {'n_input_CPO': 5, 'n_output_CPO': 5}}
+                             'critic_reg_mode': [], 'cri_reg_strength': 0, 'lambda_mix': 0, 'timers': {}, 'rnf_prop': {'n_input_CPO': 10, 'n_output_CPO': 10}}
 elif Algorithm == 'WAE':
     alg_specific_settings = {'optimizer_class': 'Adam', 'learning_rate': 1e-4, 'beta1': 0.5, 'beta2': 0.9,  
                              'rel_enc_skip_rate': 1, 'rel_cri_skip_rate': 1, 'rel_gen_skip_rate': 1, 'n_filter': 128, 'n_flat': 400, 
@@ -427,8 +427,10 @@ elif dataset_to_use == 'CIFAR10':
     parser.add_argument('--reconst_vis_epoch_rate', type=list, default=[3,1], help='reconst epoch repeat')
     parser.add_argument('--interpolate_vis_epoch_rate', type=list, default=[3,1], help='interpolation epoch repeat')
     parser.add_argument('--fixed_samples_vis_epoch_rate', type=list, default=[3,1], help='fixed samples epoch repeat')
-    parser.add_argument('--fid_inception_score_epoch_rate', type=list, default=[50,10], help='compute fid and inception score')
-    parser.add_argument('--pigeonhole_score_epoch_rate', type=list, default=[50,10], help='compute pigeonhole score')
+    # parser.add_argument('--fid_inception_score_epoch_rate', type=list, default=[50,10], help='compute fid and inception score')
+    # parser.add_argument('--pigeonhole_score_epoch_rate', type=list, default=[50,10], help='compute pigeonhole score')
+    parser.add_argument('--fid_inception_score_epoch_rate', type=list, default=[0,1], help='compute fid and inception score')
+    parser.add_argument('--pigeonhole_score_epoch_rate', type=list, default=[0,1], help='compute pigeonhole score')
     parser.add_argument('--reconstruction_score_epoch_rate', type=list, default=[10,1], help='compute reconstruction score')
     parser.add_argument('--sharpness_score_epoch_rate', type=list, default=[10,1], help='compute sharpness score')
 
