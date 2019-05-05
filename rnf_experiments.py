bd97d4f03d7bdf694195cf39324d720677f7a066
Mevlana Gemici <mevlanagemici@gmail.com>
2019-05-05 08:38:55 +0300

    bv

--- rnf_experiments.py
+++ rnf_experiments.py
@@ -60,14 +60,14 @@ range_1_min = -1
 range_1_max = 1
 
 resolution = 200
-n_samples = 10000
-n_training_samples = 10000
+n_samples = 20000
+n_training_samples = 20000
 
 n_epochs = 50
 vis_epoch_rate = 1
 
 batch_size = 500
-train_batch_size = 100
+train_batch_size = 50
 n_latent = 2
 n_out = 3
 n_input_CPO, n_output_CPO = 15, 15
@@ -113,10 +113,10 @@ x_rec, log_pdf_x_rec = serial_flow.transform(z_x, log_pdf_z_x)
 rec_cost = 100*tf.reduce_mean(tf.reduce_sum((x_rec-x_input)**2, axis=1))
 # rec_cost = tf.reduce_mean(tf.reduce_sum((x_rec-x_input)**2, axis=1))
 
-
+optimizer = tf.train.AdamOptimizer(learning_rate=0.001, beta1=0.5, beta2=0.9, epsilon=1e-08)
 # optimizer = tf.train.RMSPropOptimizer(learning_rate=0.01, decay=0.9, momentum=0., epsilon=1e-10) # good with overall rotation added
 # optimizer = tf.train.AdamOptimizer(learning_rate=0.001, beta1=0.99, beta2=0.999, epsilon=1e-08) # good with overall rotation added
-optimizer = tf.train.AdamOptimizer(learning_rate=0.01, beta1=0.5, beta2=0.9, epsilon=1e-08) # good with overall rotation added
+# optimizer = tf.train.AdamOptimizer(learning_rate=0.01, beta1=0.5, beta2=0.9, epsilon=1e-08) # good with overall rotation added
 # optimizer = tf.train.AdamOptimizer(learning_rate=0.001, beta1=0.5, beta2=0.9, epsilon=1e-08) # good without overall rotation added
 # optimizer = tf.train.AdamOptimizer(learning_rate=0.001, beta1=0.5, beta2=0.99, epsilon=1e-08) # good without overall rotation added
 cost_step = optimizer.minimize(rec_cost)
