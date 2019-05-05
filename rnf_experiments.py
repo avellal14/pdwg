3f61c3294d6718ed58e50c6534fb2ee59fabf421
Mevlana Gemici <mevlanagemici@gmail.com>
2019-05-05 08:02:26 +0300

    bv

--- rnf_experiments.py
+++ rnf_experiments.py
@@ -66,7 +66,7 @@ n_epochs = 40
 vis_epoch_rate = 1
 
 batch_size = 500
-train_batch_size = 10
+train_batch_size = 25
 n_latent = 2
 n_out = 3
 n_input_CPO, n_output_CPO = 10, 10
@@ -114,8 +114,8 @@ rec_cost = 100*tf.reduce_mean(tf.reduce_sum((x_rec-x_input)**2, axis=1))
 
 
 # optimizer = tf.train.RMSPropOptimizer(learning_rate=0.01, decay=0.9, momentum=0., epsilon=1e-10) # good with overall rotation added
-optimizer = tf.train.AdamOptimizer(learning_rate=0.0001, beta1=0.99, beta2=0.999, epsilon=1e-08) # good with overall rotation added
-# optimizer = tf.train.AdamOptimizer(learning_rate=0.01, beta1=0.5, beta2=0.9, epsilon=1e-08) # good with overall rotation added
+# optimizer = tf.train.AdamOptimizer(learning_rate=0.001, beta1=0.99, beta2=0.999, epsilon=1e-08) # good with overall rotation added
+optimizer = tf.train.AdamOptimizer(learning_rate=0.01, beta1=0.5, beta2=0.9, epsilon=1e-08) # good with overall rotation added
 # optimizer = tf.train.AdamOptimizer(learning_rate=0.001, beta1=0.5, beta2=0.9, epsilon=1e-08) # good without overall rotation added
 # optimizer = tf.train.AdamOptimizer(learning_rate=0.001, beta1=0.5, beta2=0.99, epsilon=1e-08) # good without overall rotation added
 cost_step = optimizer.minimize(rec_cost)
