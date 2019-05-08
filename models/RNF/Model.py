9c5b071ae8193948e561a2fb2ab1d3eb8cc1cd1e
Mevlana Gemici <mevlanagemici@gmail.com>
2019-05-08 17:20:19 +0300

    bv

--- models/RNF/Model.py
+++ models/RNF/Model.py
@@ -207,7 +207,7 @@ class Model():
         self.OT_primal = self.sample_distance_function(self.input_sample, self.reconst_sample)
         self.mean_OT_primal = tf.reduce_mean(self.OT_primal)
 
-        self.enc_cost = self.mean_OT_primal+self.config['enc_reg_strength']*self.enc_reg_cost
+        self.enc_cost = self.enc_reg_cost
 
         ### Generator
         self.gen_cost = self.mean_OT_primal
