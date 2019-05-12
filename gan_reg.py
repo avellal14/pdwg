83fe3982c86971049ff4db7bb36bb1238a1cd17c
Mevlana Gemici <mevlanagemici@gmail.com>
2019-05-12 22:18:40 +0300

    bv

--- gan_reg.py
+++ gan_reg.py
@@ -208,7 +208,7 @@ parser.add_argument('--restore_dir', type=str, default='/799f428b8f7e42459281ed5
 parser.add_argument('--restore', type=bool, default=False, help='Restore model.')
 parser.add_argument('--gpu', type=str, default='0', help='gpu to use.')
 parser.add_argument('--epochs', type=int, default=1000000000, help='Number of epochs to train.')
-parser.add_argument('--batch_size', type=int, default=25, help='Input batch size for training.')
+parser.add_argument('--batch_size', type=int, default=100, help='Input batch size for training.')
 parser.add_argument('--time_steps', type=int, default=1, help='Number of timesteps')
 parser.add_argument('--seed', type=int, default=1123124, help='random seed')
 
