f8816e03511baa34eaf3471a90ccd9174a20b061
Mevlana Gemici <mevlanagemici@gmail.com>
2019-05-15 03:27:15 +0300

    bv

--- models/RNF/Model.py
+++ models/RNF/Model.py
@@ -204,8 +204,8 @@ class Model():
         # self.interpolated_obs = {'flat': None, 'image': tf.reshape(self.interpolated_transformed_posterior_latent_code, [-1, 10, *batch['observed']['properties']['image'][0]['size'][2:]])}
         self.interpolated_obs = {'flat': None, 'image': tf.tile(self.input_sample['image'][:self.batch_size_tf//2, :, :, :, :], [1, 10, 1, 1, 1])}
 
-        self.cri_reg_cost = -tf.reduce_mean(self.transformed_pre_posterior_log_pdf)
-        self.enc_reg_cost = self.cri_reg_cost
+        self.enc_reg_cost = -tf.reduce_mean(self.transformed_pre_posterior_log_pdf)
+        self.cri_reg_cost = -tf.reduce_mean(self.pre_posterior_log_pdf)
 
         #############################################################################
         # REGULARIZER
@@ -221,7 +221,8 @@ class Model():
         self.OT_primal = self.sample_distance_function(self.input_sample, self.reconst_sample)
         self.mean_OT_primal = tf.reduce_mean(self.OT_primal)
 
-        self.cri_cost = -tf.reduce_mean(self.pre_posterior_log_pdf)
+        # self.cri_cost = -tf.reduce_mean(self.pre_posterior_log_pdf)
+        self.cri_cost = self.mean_OT_primal+self.config['enc_reg_strength']*self.enc_reg_cost
         self.enc_cost = self.mean_OT_primal+self.config['enc_reg_strength']*self.enc_reg_cost
         self.gen_cost = self.mean_OT_primal+self.config['enc_reg_strength']*self.enc_reg_cost
 
