a21681233ff74aa3f81517939a894f4e82f7c4a9
mevlana.gemici <mevlana.gemici@pathai.com>
2019-12-08 17:58:02 -0500

    ‘mg’

--- models/NADE/Model.py
+++ models/NADE/Model.py
@@ -152,7 +152,7 @@ class Model():
         self.mean_OT_primal = tf.reduce_mean(self.OT_primal)
 
         timescale, start_time = 5, 5
-        tradeoff = helper.hardstep((self.epoch-float(start_time))/float(timescale)+0.0001)
+        tradeoff = helper.hardstep((self.epoch-float(start_time))/float(timescale))
         overall_cost = tradeoff*self.mean_neg_log_pdf+(1-tradeoff)*self.mean_OT_primal
         # overall_cost = self.mean_OT_primal
         # overall_cost = self.mean_neg_log_pdf
